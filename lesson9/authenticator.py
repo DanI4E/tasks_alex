80e1f6d5858abf8914f5f8786fbd30a49c65ad26
DanI4E <che25d@mail.ru>
2024-10-02 00:23:52 +0700

    Homework 9

--- lesson9/authenticator.py
+++ lesson9/authenticator.py
@@ -1,74 +1,65 @@
+import os.path
 from exceptions import AuthorizationError, RegistrationError
 from datetime import datetime
 
 
 class Authenticator:
-    """Аутентификация пользователя"""
-
-    def __init__(self, login: str | None = None, _password: str | None = None, last_success_login_at: datetime | None = None, errors_count: int = 0):
+    def __init__(self, login: str | None = None, _password: str | None = None,
+                 last_success_login_at: datetime | None = None, errors_count: int = 0):
         self.login = login
         self._password = _password
         self.last_success_login_at = last_success_login_at
         self.errors_count = errors_count
+        if self._is_auth_file_exist():
+            self._read_auth_file()
 
-    
-    def _is_auth_file_exist(self):
-        try:
-            with open("C:/Python/Projects/lessons/lesson9/auth.txt"):
-                return True
-        except Exception:
-            return False
+    def _is_auth_file_exist(self) -> bool:
+        """Проверка существует ли файл auth.txt в корне проекта"""
 
+        is_file_exist = os.path.exists("auth12.txt")
+        if is_file_exist:
+            return True
+
+        return False
 
     def _read_auth_file(self):
-        with open("C:/Python/Projects/lessons/lesson9/auth.txt", "r") as f:
+        """Метод прочитывает файл и сохраняет строчки в переменные конструктора"""
+
+        with open("auth.txt", "r") as f:
             self.login = f.readline().strip()
             self._password = f.readline().strip()
             self.last_success_login_at = f.readline().strip()
-            self.errors_count = f.readline().strip()
-
-
-    def authorize(self, login: str, password: str):
-        if self.login == login and self._password == password:
-            return self.login, self._password
-        
-        if self.login == None:
-            raise AuthorizationError("Логина нет")
-        
-        self.errors_count += 1
-        self._update_auth_file()
-        raise AuthorizationError("Логин и/или пароль не соответствуют")
-
+            self.errors_count = int(f.readline().strip())
 
     def _update_auth_file(self):
-        with open("C:/Python/Projects/lessons/lesson9/auth.txt", "w") as f:
+        """Метод обновляет счетчик неудачных попыток авторизации и время авторизации"""
+
+        with open("auth.txt", "w") as f:
             f.write(self.login + '\n')
             f.write(self._password + '\n')
-            f.write(self.last_success_login_at + '\n')
-            f.write(self.errors_count)
+            f.write(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+            f.write(str(self.errors_count))
 
+    def authorize(self, login: str, password: str):
+        """Метод авторизации. Сравнивает введенные логин и пароль с логином и паролем в файле auth.txt"""
+
+        if self.login != login or self._password != password or self.login == None:
+            self.errors_count += 1
+            raise AuthorizationError("Логин и/или пароль не соответствуют")
+
+        # self.errors_count = 0
+        self._update_auth_file()
+        return "Вы авторизировались"
 
     def registrate(self, login: str, password: str):
-        if self._is_auth_file_exist():
-            raise RegistrationError("Файл auth.txt рядом")
-        
-        if self.login is not None:
-            raise RegistrationError("login не None")
-        
-        with open("C:/Python/Projects/lessons/lesson9/auth.txt", "a") as f:
+        """Метод регистрации нового пользователя"""
+
+        if self._is_auth_file_exist() or self.login is not None:
+            self.errors_count += 1
+            raise RegistrationError("Ошибка регистрации")
+
+        with open("auth12.txt", "a") as f:
             f.write(login + '\n')
             f.write(password + '\n')
-            f.write(datetime.utcnow().isoformat() + '\n')
-            f.write(self.errors_count)
-
-
-
-        
-            
-
-    
-
-
-
-
-    
+            f.write(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+            f.write(str(self.errors_count))
--- lesson9/lesson9.py
+++ lesson9/lesson9.py
@@ -1,9 +1,36 @@
+"""Lesson 9"""
+
 from authenticator import Authenticator
 import random
-# from exceptions import ValidateError
 
 
 __author__ = "Danil Cherinov"
+
+# Задачи:
+#
+# 1. Делаем систему регистрации-авторизации:
+#
+#    Создаем класс `Authenticator` в модуле `authenticator`. Методы класса:
+#    1. Конструктор. В нем создаются переменные экземпляра класса `self.login: str | None`, `self._password | None`, `self.last_success_login_at: datetime | None`, `self.errors_count: int`. По умолчанию у этих переменных должно быть `None` значение.
+#       У переменной `errors_count` значение 0. Вызывает метод `_is_auth_file_exist`. Если файл существует, вызвать метод `_read_auth_file`.
+#    2. `_is_auth_file_exist` - Проверяем наличие файла `auth22.txt` рядом (в той же папке). Не принимает аргументов, возвращает `bool` значение. `True` - файл авторизации существует. `False` - не существует.
+#    3. `_read_auth_file` - Чтение данных из файла `auth22.txt`. Данные из файла записываем в переменные объекта класса созданные ранее (`self.login`, `self._password`, etc). Ничего не возвращает. В файле должно быть 4 строки:
+#       1. Логин
+#       2. Пароль
+#       3. `datetime.utcnow().isoformat()` строка, которую нужно перевести к `datetime` объекту
+#       4. количество проваленных попыток (ошибки)
+#    4. `authorize(login, password)` - Проверка логина и пароля. Принимает аргументы строки логина и пароля. Сравнивает логин и пароль из аргументов с логином и паролем из файла. Если логин и пароль неверные, вызывает исключение `AuthorizationError`
+#       (нужно создать этот класс в соответствующем месте) и увеличиваем счетчик проваленных попыток-ошибок и перезаписываем в файле - вызывает метод `_update_auth_file`. Если `self.login` имеет `None` значение, вызвать ошибку `AuthorizationError`.
+#    5. `_update_auth_file` - Перезапись файла `auth22.txt`. Не принимает аргументов, не возвращает ничего. Метод должен перезаписать количество попыток авторизации и время авторизации, что лежат в переменных экземпляра.
+#    6. `registrate(login, password)` - Регистрация пользователя. Принимает аргументы строки логина и пароля. Делает проверку, что файла рядом `auth22.txt` нет. Если он есть, вызывает исключение `RegistrationError` (нужно создать этот класс в соответствующем месте).
+#       Создает файл `auth22.txt` и сохраняет туда логин, пароль, `datetime.utcnow().isoformat()`, количество проваленных попыток (ошибки) при попытке регистрации (Вызывает метод `_update_auth_file`). Если `self.login` имеет НЕ `None` значение, вызвать ошибку `RegistrationError`.
+# 2. В `main` функции (в файле `main.py`) создаем объект класса `Authenticator`.
+# 3. Проверяем, что у объекта класса `Authenticator` есть логин (не None значение). Если его нет, сказать пользователю, что он проходит регистрацию. Если логин есть, сказать, что нужно для авторизации вести логин и пароль.
+# 4. В бесконечном цикле запрашиваем у пользователя логи и пароль. Нужно либо зарегистрировать пользователя, либо авторизовать в зависимости от предыдущей проверки в пункте выше. Обрабатывать ошибки, вызываемые методами класса `Authenticator`.
+# 5. Удаляем весь код с подсказкой паспорта, ввода имени и возраста. Класс валидатора, модуль валидатора и ошибку валидации удаляем (но не забываем, что это должно быть все в гит истории, потому что к этому вернемся).
+# 6. Приветствуем пользователя: пишем логин, время последней успешной авторизации (формат `день.месяц.год час:минута:секунда`) и сколько раз пытались войти в приложение с ошибкой авторизации.
+# 7. Запускаем игру в отгадайку рандомного числа.
+
 
 def guess_number_game():
     """ Игра угадай число от 1 до 5 """
@@ -22,6 +49,14 @@ def guess_number_game():
         print(f"Повезет в любви, Загаданное цисло = {m}")
 
 
+# def format_timedelta(td):
+#     """ Перевод значения timedelta к format(hours, minutes, seconds)"""
+#
+#     minutes, seconds = divmod(td.seconds + td.days * 86400, 60) # divmod выводит частное и остаток
+#     hours, minutes = minutes // 60, minutes % 60
+#     return '{:d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
+
+
 def main():
     """ Выполнения функций ввода имени, ввода возраста и других функций """
 
@@ -34,32 +69,30 @@ def main():
 
     print(text)
 
-
-    # counter = 0
-    # last_attempt = 0
-
     while True:
-        # counter += 1
-        login = input('Введите логин: ')
-        password = input('Введите пароль: ')
 
-        if text == 'Необходимо пройти регистрацию':
-            authenticator.registrate(login, password)
-        else:
-            authenticator.authorize(login, password)
+        login = input('Введите логин: ').strip()
+        password = input('Введите пароль: ').strip()
 
+        try:
+            if text == 'Необходимо пройти регистрацию':
+                authenticator.registrate(login, password)
+                info = "Вы зарегистрировались!"
+            else:
+                authenticator.authorize(login, password)
+                info = (f"Привет {login.title()}! Последняя успешная авторизация в {authenticator.last_success_login_at}."
+                        f"\nВы пытались {authenticator.errors_count} раз войти в приложение с ошибкой авторизации.")
 
-        # last_attempt = counter
-        # if counter == last_attempt:
-        #     data_with_date = DataWithDate()
-        #     last_attempt = data_with_date.time
+            print(info)
 
-        print(f"Привет {login.title()}")
-            #   f"Первая попытка ввода данных была в {first_attempt.strftime('%H:%M:%S')}. "
-            #   f"Ты ввел(а) корректные данные c №{counter} попытки в {last_attempt.strftime('%H:%M:%S')}.\n"
-            #   f"Разница времени между первой и последней попыткой ввода данных: {time_difference.seconds//3600}:{(time_difference.seconds//60)%60}:{time_difference.seconds//1}")
-        guess_number_game()
+        except Exception as e:
+            print(f"Еще раз, {e}")
+            continue
+
         break
+
+    guess_number_game()
+
 
 if __name__ == '__main__':
     main()
