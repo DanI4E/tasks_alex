9fef7709641c78f5f4f29f8cdaed81cf6d615ea3
Danil Cherinov <che25@mail.ru>
2024-10-30 02:28:47 +0700

    изменил эндпоинт /age 2 вариант

--- lesson13/two_endpoints.py
+++ lesson13/two_endpoints.py
@@ -8,10 +8,9 @@ def hello():
     return "Привет, рад видеть!"
 
 
-@app.route('/age')
-def your_age():
-    b = 25
-    return f"Уже много годочков, а еще не программист ;( Просто число: {b * 2}"
+@app.route('/<int:x>')
+def your_age(x: int):
+    return f"Уже много годочков, а еще не программист ;( Просто число: {x * 2}"
 
 
 if __name__ == '__main__':
